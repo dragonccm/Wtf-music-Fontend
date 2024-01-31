ec959ff8308101b5c40901220328b1940189e28e
dragonccm <121989450+dragonccm@users.noreply.github.com>
2024-01-31 08:47:39 +0700

    modified:   package-lock.json 	modified:   package.json 	new file:   server.js 	modified:   src/App.js 	modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [apiResult, setApiResult] = useState(null);
+
+  async function fetchData() {
+    const url = 'https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem';
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '79723fdce5msh14c7e1183c905e4p15d70ejsn9d00634fb4d3',
+        'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      setApiResult(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={fetchData}>First Fetch</button>
+
+
+        {apiResult && (
+          <div>
+            <h2>API Result:</h2>
+            <pre>{JSON.stringify(apiResult, null, 2)}</pre>
+          </div>
+        )}
       </header>
+
     </div>
   );
 }
