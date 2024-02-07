32941590fecfdd82b844097ff2ae4d91013d72c8
dragonccm <121989450+dragonccm@users.noreply.github.com>
2024-02-08 00:31:55 +0700

    modified:   src/components/mainpage_playlist.jsx

--- src/components/mainpage_playlist.jsx
+++ src/components/mainpage_playlist.jsx
@@ -1,6 +1,4 @@
 import React from "react"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import "../css/mainpage.css"
 
 import "../css/Bottombar.css"
