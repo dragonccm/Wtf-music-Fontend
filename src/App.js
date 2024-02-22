d7fb20eb711c0dadba1fc6c67c4d8827d6ccbdcb
dragonccm <121989450+dragonccm@users.noreply.github.com>
2024-02-22 20:46:48 +0700

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import RightSidebar from './components/RightSidebar'
 import Bottombar from './components/Bottombar'
 import Mainpage_playlist from './components/mainpage_playlist'
 
-
+import YTMusic from "ytmusic-api"
 
 function App() {
+
   const playlistsData = Array.from({ length: 5 }, (_, index) => ({
     id: index,
     name: `Playlist ${index + 1}`,
@@ -18,12 +19,22 @@ function App() {
     list: playlistsData,
   }));
 
+
+  async function play() {
+    const ytmusic = new YTMusic()
+    await ytmusic.initialize(/* Optional: Custom cookies */)
+
+    ytmusic.search("Never gonna give you up").then(songs => {
+      console.log(songs)
+    })
+  }
   return (
     <div className="App">
       <div className="main_content">
         <RightSidebar />
         <Mainpage_playlist playlists={element} />
       </div>
+      <button onClick={play()}></button>
       <Bottombar music="https://aac.saavncdn.com/533/a4d723b40272bd6bbcb4263c61af847a_320.mp4" />
     </div>
   );
