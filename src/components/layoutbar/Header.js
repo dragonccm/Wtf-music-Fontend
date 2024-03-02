743836bf43cdde6e797400e7e51488d3e1e455f5
Hung-Thinhh <hungthinhh2003@gmail.com>
2024-03-02 19:14:51 +0700

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/layoutbar/Header.js 	modified:   src/components/sideNavigation/Bottombar.js 	modified:   src/css/Header.scss 	modified:   src/css/RightSidebar.scss 	modified:   src/css/mainpage.scss

--- src/components/layoutbar/Header.js
+++ src/components/layoutbar/Header.js
@@ -1,42 +1,89 @@
-import React from 'react';
+import React from "react";
 import "../../css/Header.scss";
-import Popup from 'reactjs-popup';
-import 'reactjs-popup/dist/index.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleLeft, faChevronRight,faUser,faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
-import logo from '../../img/logo3 (1).png'
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAngleLeft,
+  faChevronRight,
+  faUser,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
+import logo from "../../img/logo3 (1).png";
 
 const Header = () => {
-    return (
-        <div className='Header'>
-            <div className='header_wrap'>
-            <div className="Navigation">
-                <button className='Navigation_btn previous_page'>
-                    <FontAwesomeIcon icon={faAngleLeft} />
-                </button>
-                <button className='Navigation_btn next_page'>
-                    <FontAwesomeIcon icon={faChevronRight} />
-                </button>
-            </div>
-            <div className="h_avt_container">
-
-
-                <Popup trigger={<button className='avt_page'><img src={logo} alt="f" /></button>} position="bottom right"
-                    nested
-                    closeOnDocumentClick
-                    mouseLeaveDelay={300}
-                    mouseEnterDelay={0}
-                    contentStyle={{ padding: '0', border: 'none' }}
-                    arrow={false}>
-                    <div className="menu">
-                        <a href='/login' className="menu-item"><FontAwesomeIcon icon={faUser} /> Hồ sơ của bạn</a>
-                        <a href='/profile/info' className="menu-item"><FontAwesomeIcon icon={faRightFromBracket} /> đăng xuất</a>
-                    </div>
-                </Popup>
-
-            </div>
-            </div>
+  return (
+    <div className="Header">
+      <div className="header_wrap">
+        <div className="Navigation">
+          <button className="Navigation_btn previous_page">
+            <FontAwesomeIcon icon={faAngleLeft} />
+          </button>
+          <button className="Navigation_btn next_page">
+            <FontAwesomeIcon icon={faChevronRight} />
+          </button>
         </div>
-    )
-}
+        <div className="header_search">
+          <input
+            type="email"
+            id="input__search"
+            class="input__search"
+            placeholder="Tìm kiếm bài hát, nghệ sĩ, lời bài hát,.."
+            required=""
+          />
+        </div>
+        <div className="header_right">
+          <label for="theme" class="theme">
+            <span class="theme__toggle-wrap">
+              <input
+                id="theme"
+                class="theme__toggle"
+                type="checkbox"
+                role="switch"
+                name="theme"
+                value="dark"
+              />
+              <span class="theme__fill"></span>
+              <span class="theme__icon">
+                <span class="theme__icon-part"></span>
+                <span class="theme__icon-part"></span>
+                <span class="theme__icon-part"></span>
+                <span class="theme__icon-part"></span>
+                <span class="theme__icon-part"></span>
+                <span class="theme__icon-part"></span>
+                <span class="theme__icon-part"></span>
+                <span class="theme__icon-part"></span>
+                <span class="theme__icon-part"></span>
+              </span>
+            </span>
+          </label>
+          <div className="h_avt_container">
+            <Popup
+              trigger={
+                <button className="avt_page">
+                  <img src={logo} alt="f" />
+                </button>
+              }
+              position="bottom right"
+              nested
+              closeOnDocumentClick
+              mouseLeaveDelay={300}
+              mouseEnterDelay={0}
+                          contentStyle={{ padding: "0", border: "none", width: "150px", top: '55px', left:'1334px' }}
+              arrow={false}>
+              <div className="menu">
+                <a href="/login" className="menu-item">
+                  <FontAwesomeIcon icon={faUser} /> Hồ sơ của bạn
+                </a>
+                <a href="/profile/info" className="menu-item">
+                  <FontAwesomeIcon icon={faRightFromBracket} /> Đăng xuất
+                </a>
+              </div>
+            </Popup>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Header;
--- src/components/sideNavigation/Bottombar.js
+++ src/components/sideNavigation/Bottombar.js
@@ -1,37 +1,40 @@
-import React from "react"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import ReactJkMusicPlayer from 'react-jinke-music-player';
+import 'react-jinke-music-player/assets/index.css';
+import "../../css/Bottombar.scss";
+import { createRoot } from 'react-dom/client';
 
-import AudioPlayer from 'react-h5-audio-player';
-import 'react-h5-audio-player/lib/styles.css';
-
-import "../../css/Bottombar.scss"
 const Bottombar = (props) => {
-    return (
-        <div className="main_bottom_bar">
-            <div className="bottom_bar">
-                <div className="left">
-                    <div className="info_img">
-                        <img src="https://lh3.googleusercontent.com/03WLPin15fkpkLMayBhp4E1YyOJRtcwOL8r2albpcYfmva2vqNbr4RM8q8zk5pGhu9PHdWHk60d2haTOPdgghaUxpDFO4ROPSg=w1600-rj" alt="f" className="song_avt" />
-                    </div>
-                    <div className="info">
-                        <h3>Flower</h3>
-                        <p>JISOO</p>
-                    </div>
-                    <FontAwesomeIcon icon={faHeart} />
-                </div>
-                <div className="center">
-                        <AudioPlayer
-                            autoPlay
-                            src={props.music}
-                            onPlay={e => console.log("onPlay")}
-                        />
-                </div>
-                <div className="right">
-                    <p>một cái gì đó tao sẽ làm </p>
-                </div>
-            </div>
+  return (
+    <div className="main_bottom_bar">
+      <div className="bottom_bar">
+        <div className="left">
+          <div className="info_img">
+            <img src="https://lh3.googleusercontent.com/03WLPin15fkpkLMayBhp4E1YyOJRtcwOL8r2albpcYfmva2vqNbr4RM8q8zk5pGhu9PHdWHk60d2haTOPdgghaUxpDFO4ROPSg=w1600-rj" alt="f" className="song_avt" />
+          </div>
+          <div className="info">
+            <h3>Flower</h3>
+            <p>JISOO</p>
+          </div>
+          <FontAwesomeIcon icon={faHeart} />
         </div>
-    )
-}
-export default Bottombar
+        <div className="center">
+          <div id="music-player"></div>
+        </div>
+        <div className="right">
+          <p>một cái gì đó tao sẽ làm </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+createRoot(document.getElementById('root')).render(
+  <ReactJkMusicPlayer
+  musicSrc={'https://a128-zmp3.zmdcdn.me/12fb41f934c32cb856933163a2bad73b?authen=exp=1709544563~acl=/12fb41f934c32cb856933163a2bad73b/*~hmac=6c44dee6746a3411964ab49ec3e055da'}
+  />
+);
+
+export default Bottombar;
