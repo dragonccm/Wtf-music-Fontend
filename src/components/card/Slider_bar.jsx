15b993cedec8d624cd0ec13a2bda7ef2bd3c8988
dragonccm <121989450+dragonccm@users.noreply.github.com>
2024-03-04 09:25:56 +0700

    .

--- src/components/card/Slider_bar.jsx
+++ src/components/card/Slider_bar.jsx
@@ -36,13 +36,13 @@ const SliderBar = () => {
         <div className="slider-container">
           <Slider {...settings}>
             <div className="slider_item">
-              <img src="https://photo-zmp3.zmdcdn.me/banner/2/e/c/1/2ec1e2bacee5bf84e3bc5322ac2cf764.jpg"></img>
+              <img src="https://photo-zmp3.zmdcdn.me/banner/2/e/c/1/2ec1e2bacee5bf84e3bc5322ac2cf764.jpg" alt="f"></img>
             </div>
             <div className="slider_item">
-              <img src="https://photo-zmp3.zmdcdn.me/banner/d/7/c/1/d7c15b798d6d71431a1b671ff3b65d03.jpg"></img>
+              <img src="https://photo-zmp3.zmdcdn.me/banner/d/7/c/1/d7c15b798d6d71431a1b671ff3b65d03.jpg" alt="f"></img>
             </div>
             <div className="slider_item">
-              <img src="https://photo-zmp3.zmdcdn.me/banner/0/4/e/5/04e56051f643d0758bb4d90853fd79e6.jpg"></img>
+              <img src="https://photo-zmp3.zmdcdn.me/banner/0/4/e/5/04e56051f643d0758bb4d90853fd79e6.jpg" alt="f"></img>
             </div>
             
            
