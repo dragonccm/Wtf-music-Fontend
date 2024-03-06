f9415d4db55e70fcaff01becf5fa2295648377b5
dragonccm <121989450+dragonccm@users.noreply.github.com>
2024-03-06 08:15:14 +0700

    cc

--- src/components/pages/top100.jsx
+++ src/components/pages/top100.jsx
@@ -6,22 +6,23 @@ const Top100 = ({ data }) => {
     return (
         <div className="top100_main">
             <div className="main_banner">
-                <h1>cái banner gì đó</h1>
+                <h1>TOP 100</h1>
             </div>
+            <h1 className="catego_title">Nhạc việt</h1>
             <div className="list_card">
-                <h1>Có thể bạn muốn nghe</h1>
                 <Card playlist={data} />
             </div>
+            <h1 className="catego_title">US-UK</h1>
             <div className="list_card">
-                <h1>Có thể bạn muốn nghe</h1>
+                <Card playlist={data} />
                 <Card playlist={data} />
             </div>
+            <h1 className="catego_title">KPOP</h1>
             <div className="list_card">
-                <h1>Có thể bạn muốn nghe</h1>
                 <Card playlist={data} />
             </div>
+            <h1 className="catego_title">phonk</h1>
             <div className="list_card">
-                <h1>Có thể bạn muốn nghe</h1>
                 <Card playlist={data} />
             </div>
         </div>
