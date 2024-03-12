9288c76d0929070b53e88ded85212e1bd7080b79
lyngocminhthien <lyngocminhthien@egmail.com>
2024-03-12 08:21:16 +0700

    6969

--- src/setup/axios.js
+++ src/setup/axios.js
@@ -1,44 +1,53 @@
 import axios from "axios";
-import { toast } from "react-toastify"
+import { toast } from "react-toastify";
 const instance = axios.create({
-    baseURL: 'http://localhost:8081'
+  baseURL: "http://localhost:6969",
 });
 
-
-instance.defaults.withCredentials = true; 
+instance.defaults.withCredentials = true;
 // // Alter defaults after instance has been created
-instance.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('jwt')}`;
+instance.defaults.headers.common[
+  "Authorization"
+] = `Bearer ${localStorage.getItem("jwt")}`;
 
 // Add a request interceptor
-instance.interceptors.request.use(function (config) {
+instance.interceptors.request.use(
+  function (config) {
     // Do something before request is sent
     return config;
-  }, function (error) {
+  },
+  function (error) {
     // Do something with request error
     return Promise.reject(error);
-});
-  // Add a response interceptor
-  instance.interceptors.response.use(function (response) {
+  }
+);
+// Add a response interceptor
+instance.interceptors.response.use(
+  function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     return response.data;
-  }, function (error) {
+  },
+  function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     const status = error.response?.status || 500;
     // we can handle global errors here
     switch (status) {
       // authentication (token related issues)
       case 401: {
-        if (window.location.pathname !== '/' && window.location.pathname !== '/login' && window.location.pathname !== '/register') {
-          
-          toast.error('Not authenticated the user')
+        if (
+          window.location.pathname !== "/" &&
+          window.location.pathname !== "/login" &&
+          window.location.pathname !== "/register"
+        ) {
+          toast.error("Not authenticated the user");
         }
         return error.response.data;
       }
 
       // forbidden (permission related issues)
       case 403: {
-        toast.error(`You don't have permission to access this resource...`)
+        toast.error(`You don't have permission to access this resource...`);
         return Promise.reject(error);
       }
 
@@ -68,6 +77,7 @@ instance.interceptors.request.use(function (config) {
       }
     }
     // return Promise.reject(error);
-  });
+  }
+);
 
 export default instance;
