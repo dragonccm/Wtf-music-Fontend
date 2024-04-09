2f96f13d03615d5c75eb8a7a5b3586dc01c9d048
dragonccm <121989450+dragonccm@users.noreply.github.com>
2024-04-09 14:22:24 +0700

    modified:   src/components/card/col_3_layout.jsx 	modified:   src/components/card/song_card.jsx

--- src/components/card/col_3_layout.jsx
+++ src/components/card/col_3_layout.jsx
@@ -1,12 +1,18 @@
 import '../../css/col_3_layout.scss';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import { NavLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { fetchSongPlaying } from "../../redux/slide/songPlayingSlice";
 import SongCard from './song_card'
-
+import { postPlayList } from '../../redux/slide/createPlayList'
+import { useSelector } from "react-redux";
 
 
 
 const Col3Layout = ({ data }) => {
   const slicedData = data.slice(0, 12);
+  const currName = useSelector((state) => state.Authentication.defaultUser.account.username);
   // Chia data thành các mảng con có 3 phần tử
   const chunkedData = [];
   const chunkSize = 4;
@@ -16,7 +22,22 @@ const Col3Layout = ({ data }) => {
     chunkedData.push(chunk);
   }
 
-
+  const handlePlaying = (e, id) => {
+    e.preventDefault();
+    dispatch(fetchSongPlaying(id));
+    dispatch(postPlayList({
+      playlistname: currName,
+      genresid: [
+      ],
+      artistsId: currName,
+      thumbnail: "",
+      description: '',
+      songid: [
+          id
+      ]
+  }
+  ));
+  }
 
   return (
     data && Array.isArray(data) &&
