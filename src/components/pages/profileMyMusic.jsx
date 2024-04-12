b4cd0d78f9430679c4e0977f5e2a4022a3e8eeac
lyngocminhthien <lyngocminhthien@egmail.com>
2024-04-12 09:16:19 +0700

    ...

--- src/components/pages/profileMyMusic.jsx
+++ src/components/pages/profileMyMusic.jsx
@@ -1,10 +1,9 @@
 import Recommended from "../card/Recommended";
 import Card from "../card/playlist_card";
 import { useState } from "react";
-import '../../css/profileMyMusic.scss'
+import "../../css/profileMyMusic.scss";
 
-
-const ProfileMyMusic = ({type}) => {
+const ProfileMyMusic = ({ type }) => {
     const Recommendeds = [
         {
             encodeId: "Z7ZE00BE",
@@ -12,10 +11,12 @@ const ProfileMyMusic = ({type}) => {
             thumbnailM:
                 "https://th.bing.com/th/id/OIP.MfAV8J9NzWpF06S-jLvakQHaLH?rs=1&pid=ImgDetMain",
             category: "playlist",
-            artists: [{
-                alias: 'Phan Mạnh Cường',
-                name: 'Phan Mạnh Cường'
-            }],
+            artists: [
+                {
+                    alias: "Phan Mạnh Cường",
+                    name: "Phan Mạnh Cường",
+                },
+            ],
             songname: "chuyến xe thùng",
             addedday: "11 thg 11, 2021",
             liked_state: false,
@@ -29,10 +30,12 @@ const ProfileMyMusic = ({type}) => {
             thumbnailM:
                 "https://th.bing.com/th/id/OIP.MfAV8J9NzWpF06S-jLvakQHaLH?rs=1&pid=ImgDetMain",
             category: "playlist",
-            artists: [{
-                alias: 'Phan Mạnh Cường',
-                name: 'Phan Mạnh Cường'
-            }],
+            artists: [
+                {
+                    alias: "Phan Mạnh Cường",
+                    name: "Phan Mạnh Cường",
+                },
+            ],
             songname: "chuyến xe thùng",
             addedday: "11 thg 11, 2021",
             liked_state: false,
@@ -46,10 +49,12 @@ const ProfileMyMusic = ({type}) => {
             thumbnailM:
                 "https://th.bing.com/th/id/OIP.MfAV8J9NzWpF06S-jLvakQHaLH?rs=1&pid=ImgDetMain",
             category: "playlist",
-            artists: [{
-                alias: 'Phan Mạnh Cường',
-                name: 'Phan Mạnh Cường'
-            }],
+            artists: [
+                {
+                    alias: "Phan Mạnh Cường",
+                    name: "Phan Mạnh Cường",
+                },
+            ],
             songname: "chuyến xe thùng",
             addedday: "11 thg 11, 2021",
             liked_state: false,
@@ -63,10 +68,12 @@ const ProfileMyMusic = ({type}) => {
             thumbnailM:
                 "https://th.bing.com/th/id/OIP.MfAV8J9NzWpF06S-jLvakQHaLH?rs=1&pid=ImgDetMain",
             category: "playlist",
-            artists: [{
-                alias: 'Phan Mạnh Cường',
-                name: 'Phan Mạnh Cường'
-            }],
+            artists: [
+                {
+                    alias: "Phan Mạnh Cường",
+                    name: "Phan Mạnh Cường",
+                },
+            ],
             songname: "chuyến xe thùng",
             addedday: "11 thg 11, 2021",
             liked_state: false,
@@ -80,10 +87,12 @@ const ProfileMyMusic = ({type}) => {
             thumbnailM:
                 "https://th.bing.com/th/id/OIP.MfAV8J9NzWpF06S-jLvakQHaLH?rs=1&pid=ImgDetMain",
             category: "playlist",
-            artists: [{
-                alias: 'Phan Mạnh Cường',
-                name: 'Phan Mạnh Cường'
-            }],
+            artists: [
+                {
+                    alias: "Phan Mạnh Cường",
+                    name: "Phan Mạnh Cường",
+                },
+            ],
             songname: "chuyến xe thùng",
             addedday: "11 thg 11, 2021",
             liked_state: false,
@@ -91,7 +100,6 @@ const ProfileMyMusic = ({type}) => {
                 "https://aac.saavncdn.com/533/a4d723b40272bd6bbcb4263c61af847a_320.mp4",
             duration: "170",
         },
-
     ];
     const usserplaylist = [
         {
@@ -114,26 +122,34 @@ const ProfileMyMusic = ({type}) => {
         <div className="history_ctn">
             <Recommended
                 datas={data}
-                type={type=='mymusic'?"Bài hát yêu thích":'Bài hát đã nghe'}
-                describe={type=='mymusic'?"Bài hát yêu thích":'Bài hát đã nghe'}
+                type={
+                    type == "mymusic" ? "Bài hát yêu thích" : "Bài hát đã nghe"
+                }
+                describe={
+                    type == "mymusic" ? "Bài hát yêu thích" : "Bài hát đã nghe"
+                }
                 maxItemsToShow="5"
             />
         </div>
     );
     const Myplaylist = ({ datas }) => (
         <section className="mylist_page">
-            <div className="Recommended_1">{type=='mymusic'?"Playlist-Album yêu thích":'Playlist-Album đã nghe'}</div>
+            <div className="Recommended_1">
+                {type == "mymusic"
+                    ? "Playlist-Album yêu thích"
+                    : "Playlist-Album đã nghe"}
+            </div>
             <div className="list_container">
                 <Card playlist={datas} />
             </div>
         </section>
     );
-    const [area, setArea] = useState('song')
+    const [area, setArea] = useState("song");
 
     const handleChange = (e) => {
         console.log(e.target.value); // In ra giá trị của radio button được chọn
-        setArea(e.target.value)
-    }
+        setArea(e.target.value);
+    };
     return (
         <div className="like_song">
             <div className="radio-inputs">
@@ -142,14 +158,12 @@ const ProfileMyMusic = ({type}) => {
                         className="radio-input "
                         type="radio"
                         name="engine"
-                        value='song'
-                        checked={area === 'song'}
+                        value="song"
+                        checked={area === "song"}
                         onChange={(e) => handleChange(e)}
                     />
                     <span className="radio-tile ">
-                        <span className="radio-icon">
-                            Bài hát
-                        </span>
+                        <span className="radio-icon">Bài hát</span>
                     </span>
                 </label>
                 <label>
@@ -157,23 +171,21 @@ const ProfileMyMusic = ({type}) => {
                         className="radio-input "
                         type="radio"
                         name="engine"
-                        value='playlist'
-                        checked={area === 'playlist'}
-
+                        value="playlist"
+                        checked={area === "playlist"}
                         onChange={(e) => handleChange(e)}
-
                     />
                     <span className="radio-tile ">
-                        <span className="radio-icon">
-                            Playlist
-                        </span>
+                        <span className="radio-icon">Playlist</span>
                     </span>
                 </label>
             </div>
-            {area === 'song'?<Likesong data={Recommendeds} />: <Myplaylist datas={usserplaylist} />}
-            
-           
+            {area === "song" ? (
+                <Likesong data={Recommendeds} />
+            ) : (
+                <Myplaylist datas={usserplaylist} />
+            )}
         </div>
-    )
-}
-export default ProfileMyMusic
+    );
+};
+export default ProfileMyMusic;
