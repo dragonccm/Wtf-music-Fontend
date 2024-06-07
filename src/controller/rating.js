4a01d3cba0f354e0b3ae48f16161c43cc4fbd77a
Hung-Thinhh <hungthinhh2003@gmail.com>
2024-06-08 03:15:40 +0700

    rating

--- src/controller/rating.js
+++ src/controller/rating.js
@@ -3,7 +3,10 @@ import axios from "../setup/axios";
 const getRating = () => {
     return axios.get(`/api/rating`)
 }
+const getRankCliend = () => {
+    return axios.get(`/api/getRankCliend`)
+}
 
 export {
-    getRating
+    getRating,getRankCliend
 }
