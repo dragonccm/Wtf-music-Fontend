9ab11824d0b5361fd31dd4398a8fd25dda2e5818
dragonccm <121989450+dragonccm@users.noreply.github.com>
2024-12-26 22:53:13 +0700

    Changes to be committed: 	modified:   src/setup/axios.js 	modified:   src/socket.js

--- src/setup/axios.js
+++ src/setup/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify"
 
 const instance = axios.create({
-    baseURL: 'http://localhost:6969'
+    baseURL: 'backend-tfcopy-production.up.railway.app'
 });
 
 
