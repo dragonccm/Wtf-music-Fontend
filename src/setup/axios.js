e7eb47c760b9c2ddc6b52e68c7eb7e2c1e54eff4
dragonccm <121989450+dragonccm@users.noreply.github.com>
2024-12-26 23:27:31 +0700

    https://backend-tfcopy-production.up.railway.app/

--- src/setup/axios.js
+++ src/setup/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify"
 
 const instance = axios.create({
-    baseURL: 'https://backend-tfcopy-production.up.railway.app/'
+    baseURL: 'https://backend-tfcopy-production.up.railway.app'
 });
 
 
